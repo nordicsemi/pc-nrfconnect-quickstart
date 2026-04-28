b5e387d06a636f5a721f212dd17d5c0f6460d9af
Simen S. Røstad <simen.rostad@nordicsemi.no>
2026-04-28 15:35:58 +0200

    nrf91x: Update quickstart with new ATT version
    - Update ATT version to version 1.4.0 for Thingy91x and nRF9151DK,
      nRF9161DK is kept as is due to not being supported anymore in ATT.
    - Update dead link
    
    Signed-off-by: Simen S. Røstad <simen.rostad@nordicsemi.no>

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-export const PROVISIONING_LINK =
-    'https://docs.nordicsemi.com/bundle/asset-tracker-template-latest/page/common/provisioning.html';
+export const CONNECTING_LINK =
+    'https://docs.nordicsemi.com/bundle/asset-tracker-template-latest/page/common/connecting.html';
 
 export const ACADEMY_EXERCISE_LINK =
     'https://academy.nordicsemi.com/courses/cellular-iot-fundamentals/lessons/lesson-1-cellular-fundamentals/topic/lesson-1-exercise-1/';
