3ee743ba809fb1afedac04ecf9486240bb696e85
SlavaNordic <265652304+SlavaNordic@users.noreply.github.com>
2026-05-18 15:58:22 +0200

    Use empty string as a default app name at evaluate step (#415)
    * empty
    
    * add min height

--- src/common/Resource.tsx
+++ src/common/Resource.tsx
@@ -99,7 +99,7 @@ export const AppResourceButton = ({
     onInstallFinish?: () => void;
 }) => {
     const device = useAppSelector(getSelectedDeviceUnsafely);
-    const [displayName, setDisplayName] = useState(app);
+    const [displayName, setDisplayName] = useState('');
     const [isInstalling, setIsInstalling] = useState(false);
     const path =
         vComIndex !== undefined
@@ -116,7 +116,7 @@ export const AppResourceButton = ({
     return (
         <div className="tw-flex tw-flex-row tw-items-center tw-justify-between tw-gap-10">
             <div className="tw-w-80">
-                <div>
+                <div className="tw-min-h-5">
                     <b>{title || displayName}</b>
                 </div>
                 {description}
