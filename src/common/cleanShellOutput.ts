abdcf69d45405ab94882925af35af07490b8ad20
Tsvetelin Petrov <42267885+tsvetelinpetrov@users.noreply.github.com>
2026-07-17 10:27:46 +0200

    Replace removeAnsi from Shared with a custom removal

--- src/common/cleanShellOutput.ts
+++ src/common/cleanShellOutput.ts
@@ -4,9 +4,14 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import { removeAnsi } from '@nordicsemiconductor/pc-nrfconnect-shared';
+// TODO: Replace this with a library that can handle ANSI removal.
+const ansiPattern = [
+    '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
+    '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))',
+].join('|');
 
 export const cleanShellOutput = (raw: string) =>
-    removeAnsi(raw)
+    raw
+        .replace(new RegExp(ansiPattern, 'g'), '')
         .replace(/uart:~\$ ?/g, '') // Zephyr shell prompt
         .replace(/^> ?/gm, ''); // command-echo prompt
