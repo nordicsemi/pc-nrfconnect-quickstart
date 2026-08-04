a45db2edceab4faeb3af3622cce4445baa7f6a4a
Tsvetelin Petrov <42267885+tsvetelinpetrov@users.noreply.github.com>
2026-08-04 12:14:16 +0200

    Add ncsAddon for cloud FW

--- src/features/flows/nRF54L15_cloud/index.ts
+++ src/features/flows/nRF54L15_cloud/index.ts
@@ -31,6 +31,8 @@ const programConfig = [
             label: 'Bluetooth: Peripheral Memfault Diagnostic Service (MDS)',
             href: 'https://nrfconnectdocs.nordicsemi.com/ncs/latest/nrf/samples/bluetooth/peripheral_mds/README.html',
         },
+        ncsAddon:
+            'app=quickstart-bluetooth&branch=main&manifest=&repo=https://github.com/nrfconnect/quickstart-bluetooth',
         programmingOptions: {
             firmwareList: [
                 {
@@ -97,7 +99,7 @@ const learnConfig = [
 const developConfig = [
     {
         ref: 'Cloud Connectivity',
-        sampleSource: 'nrf/samples/bluetooth/peripheral_lbs',
+        sampleSource: '',
     },
     {
         ref: 'Hello World',
