96dd4cd891059f7fb7f542014a470ec5f40aa7d7
SlavaNordic <265652304+SlavaNordic@users.noreply.github.com>
2026-08-12 17:22:41 +0200

    eslint fix

--- src/features/flows/index.tsx
+++ src/features/flows/index.tsx
@@ -5,10 +5,11 @@
  */
 
 import type React from 'react';
+
 import NRF52 from './nRF52';
 import NRF54L15 from './nRF54L15';
-import NRF54LM20 from './nRF54LM20';
 import NRF54LC10 from './nRF54LC10';
+import NRF54LM20 from './nRF54LM20';
 import NRF54LS05 from './nRF54LS05';
 import NRF54LV10 from './nRF54LV10';
 import NRF5340 from './nRF5340';
